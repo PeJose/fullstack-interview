8b5573a7c23844b6441edd6dd0927e88afa30236
Maciej <m.stolarczyk@fireup.pro>
2025-03-04 10:32:20 +0100

    added helmet and diagram

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import membershipRoutes from "./modern/routes/membership.routes";
+import helmet from "helmet";
 import { errorHandler, requestLogger } from "./middleware";
+import membershipRoutes from "./modern/routes/membership.routes";
 
 // because of the javascript module, we need to use require to import the legacy routes
 // eslint-disable-next-line @typescript-eslint/no-require-imports
@@ -9,6 +10,7 @@ const legacyMembershipRoutes = require("./legacy/routes/membership.routes");
 const server = express();
 
 server.use(express.json());
+server.use(helmet())
 
 // logger
 server.use(requestLogger);
